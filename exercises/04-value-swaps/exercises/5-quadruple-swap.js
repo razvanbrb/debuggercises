094164a3f6c3bb50ad15b5cc8f514a2024ea4368
Razvan <brb_end1@yahoo.com>
2020-06-18 06:26:41 +0200

    solved 5-quadruple-swap

--- exercises/04-value-swaps/exercises/5-quadruple-swap.js
+++ exercises/04-value-swaps/exercises/5-quadruple-swap.js
@@ -7,6 +7,11 @@ let d = 2;
 let temp = null;
 
 // write some code
+temp=a;
+a=b;
+b=d;
+d=c;
+c=temp;
 
 
 
@@ -25,6 +30,6 @@ console.assert(isTrue3, 'Test 3');
 const isTrue4 = d === 4;
 console.assert(isTrue4, 'Test 4');
 
-const isTrue5 = temp === _;
+const isTrue5 = temp === 3;
 console.assert(isTrue5, 'Test 5');
 
