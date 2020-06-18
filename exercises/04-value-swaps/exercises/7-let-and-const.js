9caa187a2d0d78602694b939f8652bde7c15235a
Razvan <brb_end1@yahoo.com>
2020-06-18 06:41:11 +0200

    solved all from exercise 4

--- exercises/04-value-swaps/exercises/7-let-and-const.js
+++ exercises/04-value-swaps/exercises/7-let-and-const.js
@@ -13,7 +13,9 @@ temp = a;
 a = b;
 const d = a;
 // write your code below here
-
+a=c;
+b=temp;
+c= a+ b;
 
 
 
@@ -28,9 +30,9 @@ console.assert(isTrue2, 'Test 2');
 const isTrue3 = c === 3;
 console.assert(isTrue3, 'Test 3');
 
-const isTrue4 = d === _;
+const isTrue4 = d === c;
 console.assert(isTrue4, 'Test 4');
 
-const isTrue5 = temp === _;
+const isTrue5 = temp === b;
 console.assert(isTrue5, 'Test 5');
 
